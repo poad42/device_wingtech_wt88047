0112122a8945804f251eac1d451e11d8c308bcee
Prema Chand Alugu <premaca@gmail.com>
2016-08-04 05:03:36 +0530

    ROM is 4.9, just to be explicit

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,10 @@
 # Kernel
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Tool Chain
+TARGET_GCC_VERSION_EXP := 4.9
+
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_kernel_defconfig
 
