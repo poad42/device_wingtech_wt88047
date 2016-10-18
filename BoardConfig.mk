11b75b1909029b4940d3679be9d4993e250500da
Prema Chand Alugu <premaca@gmail.com>
2016-10-18 15:02:21 +0530

    kernel tool chain is 4.8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := wt88047_kernel_defconfig
 
 # Kernel Tool Chain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Tool Chain
