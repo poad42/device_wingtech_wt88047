86f0a4bc6ec6cecbfdbfceb8cf89c164ed86dd2c
nicknitewolf <nickradiantwtf@gmail.com>
2016-12-04 10:25:38 +0000

    wt88047: replace mpdecision with perfd
    By doing so, we do not need a custom power init for wt88047 and we can commonize to use msm8916-common power init.

---
+++ board/kernel.mk
@@ -0,0 +1,8 @@
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
+TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
+
+BOARD_KERNEL_CMDLINE += \
+    sched_enable_hmp=1 \
+    phy-msm-usb.floated_charger_enable=1 \
+    androidboot.selinux=permissive
