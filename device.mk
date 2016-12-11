a2c74c2d70905d57e0c3e948b34f065e6a2fc7a2
poad42 <me@adhityamohan.in>
2016-12-11 08:44:02 +0000

    wt88047: Use proper hwui/heap overrides * We have 2GB RAM.

--- device.mk
+++ device.mk
@@ -29,7 +29,8 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
